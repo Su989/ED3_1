3edd1c012b8bf1fb66bf6fc7da3ffb741c1aaf7c
Susana <susanaminguez1989@hotmail.com>
2024-11-16 00:51:14 +0100

    "Añadido mensaje de bienvenida".

--- Hola.java
+++ Hola.java
@@ -1,5 +1,6 @@
 public class Hola {
             public static void main(String[ ] args) {
                         System.out.println("Hola");
+                        System.out.println("Bienvenidos al curso de DAM");
             }
 }
